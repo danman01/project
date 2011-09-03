0b247feee594fe4b9d30b286246a53a3aa770a9d
Danny <danny.kirschner@gorankem.com>
2011-09-03 17:26:11 -0400

    updating migrations for heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,12 @@ class HomeController < ApplicationController
     @invoices=current_user.invoices
     #@invoices.reject!{|x|x.sales[0].nil?} 
     @invoices.each do |x|
-      @invoices.delete_if x.sales.empty?
+      if x.sales.empty?
+        b=1
+      end
+      if b==1
+        @invoices.delete(x)
+      end
     end    
     @events=current_user.events
    
