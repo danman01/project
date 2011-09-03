dcd86cb6f38613f85f67253cd0758ca0c6da3651
Danny <danny.kirschner@gorankem.com>
2011-09-03 15:58:07 -0400

    updating migrations for heroku

--- db/migrate/20110626174034_change_on_tour_to_on_tour_until.rb
+++ db/migrate/20110626174034_change_on_tour_to_on_tour_until.rb
@@ -1,5 +1,4 @@
 class ChangeOnTourToOnTourUntil < ActiveRecord::Migration
-  20110626174034_change_on_tour_to_on_tour_until.rb
   def self.up
 	remove_column :artists, :on_tour_until
 	add_column :artists, :on_tour_until, :datetime
